b3817b9f11a36a391509bb2c013006c1d0ffb259
Nizam <ramazanov570633@gmail.com>
2024-02-04 21:13:19 +0400

    UI- update

--- lib/src/Container/SplasFeature/Splaspage.dart
+++ lib/src/Container/SplasFeature/Splaspage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+// import '../../../src/assets/imge/im.svg';
+const String assetsonbor = '../../../src/assets/imge/im.svg';
 
 class Splaspage extends StatefulWidget {
   const Splaspage({super.key});
@@ -11,14 +15,16 @@ class _SplaspageState extends State<Splaspage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Column(
           children: [
-            Image.asset(
-              './lip/src/asset/imge/image 3.svg',
+            SvgPicture.asset(
+              assetsonbor,
               width: 200,
-              height: 200,
-            )
+              height: 600,
+            ),
+            const Text('salam'),
           ],
         ),
       ),
