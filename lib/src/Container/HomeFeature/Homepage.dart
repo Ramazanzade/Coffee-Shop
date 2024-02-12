abbc625fd9d68f45b74f529512c3de9d0865b859
Nizam <ramazanov570633@gmail.com>
2024-02-12 19:58:47 +0400

    UI- update/11

--- lib/src/Container/HomeFeature/Homepage.dart
+++ lib/src/Container/HomeFeature/Homepage.dart
@@ -1,18 +1,19 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold(
-//       // appBar:Text('salam') ,
-//       body: Text('salam'),
-//     );
-//   }
-// }
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return const Scaffold(
+      body: Text('salam'),
+    );
+  }
+}
